3d76f327eeb7c2a90e2097e16c4df9a4fc577ce4
technote <technote.space@gmail.com>
2019-01-28 21:11:06 +0900

    table prefix の取得を public transaztion追加

--- src/classes/models/db.php
+++ src/classes/models/db.php
@@ -300,7 +300,7 @@ class Db implements \WP_Framework_Core\Interfaces\Singleton, \WP_Framework_Core\
 	/**
 	 * @return string
 	 */
-	private function get_table_prefix() {
+	public function get_table_prefix() {
 		global $table_prefix;
 
 		return $table_prefix . $this->get_slug( 'table_prefix', '_' );
@@ -353,28 +353,23 @@ class Db implements \WP_Framework_Core\Interfaces\Singleton, \WP_Framework_Core\
 		if ( ! $this->need_to_update() ) {
 			return;
 		}
+
 		$this->do_action( 'start_db_update' );
-		$this->update_db_version();
-
-		if ( empty( $this->table_defines ) ) {
-			$this->do_action( 'finished_db_update' );
-			return;
-		}
-
-		set_time_limit( 60 * 5 );
-
-		foreach ( $this->table_defines as $table => $define ) {
-			$results = $this->table_update( $table, $define );
-			if ( $results ) {
-				$message = implode( '<br>', array_filter( $results, function ( $d ) {
-					return ! empty( $d );
-				} ) );
-				if ( $message ) {
-					$this->app->add_message( $message, 'db', false, false );
+		$this->transaction( function () {
+			$this->update_db_version();
+			set_time_limit( 60 * 5 );
+			foreach ( $this->table_defines as $table => $define ) {
+				$results = $this->table_update( $table, $define );
+				if ( $results ) {
+					$message = implode( '<br>', array_filter( $results, function ( $d ) {
+						return ! empty( $d );
+					} ) );
+					if ( $message ) {
+						$this->app->add_message( $message, 'db', false, false );
+					}
 				}
 			}
-		}
-
+		} );
 		$this->do_action( 'finished_db_update' );
 	}
 
