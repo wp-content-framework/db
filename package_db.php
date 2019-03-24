d846344778685f6f0d42cfc26e4781e93eb98a3c
Technote <technote.space@gmail.com>
2019-03-24 14:48:13 +0900

    v0.0.17

--- package_db.php
+++ package_db.php
@@ -2,7 +2,7 @@
 /**
  * WP_Framework Package Db
  *
- * @version 0.0.14
+ * @version 0.0.17
  * @author Technote
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
