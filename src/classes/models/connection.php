12a0e4f8736790d97f3e9c44132c7eb42cfac6ed
Technote <technote.space@gmail.com>
2019-04-06 16:50:11 +0900

    v0.0.19

--- src/classes/models/connection.php
+++ src/classes/models/connection.php
@@ -2,7 +2,7 @@
 /**
  * WP_Framework_Db Classes Models Connection
  *
- * @version 0.0.14
+ * @version 0.0.19
  * @author Technote
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
